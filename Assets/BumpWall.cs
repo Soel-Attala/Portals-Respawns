088686bf204685b654da3aca5ddda65146c48ef2
Soel Antonio Attala <95297206+Soel-Attala@users.noreply.github.com>
2022-07-10 19:19:44 -0300

    agregamos script y colisiones a la escena junto con los respawns de los objetos

---
+++ Assets/BumpWall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpWall : MonoBehaviour
+{
+    Vector3 posInicial;
+
+    void Start()
+    {
+        posInicial = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void MoverPared()
+    {
+        posInicial = transform.position = new Vector3(-0.4f, 8.265f, 10);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.gameObject.name == "Player")
+        {
+            MoverPared();
+        }
+
+               
+    }
+
+
+}
---
+++ Assets/Scripts/Colisiones.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisiones : MonoBehaviour
+{
+    Vector3 escalaInicial;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        escalaInicial = transform.localScale;
+    }
+
+    void OnTriggerEnter(Collider col) 
+    {
+        if(col.transform.gameObject.name == "BumpWall") 
+        {
+            Debug.Log("colisione con la pared dorada");
+            
+        }
+
+        if(col.transform.gameObject.name == "Floor")
+        {
+            Debug.Log("colisionaste con el piso");
+        }
+
+        if(col.transform.gameObject.name == "ShrinkerWall")
+        {
+            ReducirEscalaJugador();
+            Debug.Log("colisionaste con el portal");
+
+        }
+        
+        void ReducirEscalaJugador() 
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+
+
+    }
+}
